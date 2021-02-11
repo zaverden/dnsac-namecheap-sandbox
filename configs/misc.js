afe7c9230d2c2ca77130bf9bc98bf47c049f9f8a
Denis Zavershinskiy <zaverden@gmail.com>
2021-02-12 00:34:06 +0700

    add traefik

--- configs/misc.js
+++ configs/misc.js
@@ -1,6 +1,7 @@
 D_EXTEND(
   "zaverden.com",
   A("hole", "178.62.198.107"),
+  CNAME("traefik.hole", "hole.zaverden.com."),
   CNAME("www", "zaverden.github.io."),
   CNAME("ffw", "ghs.google.com."),
   CNAME("qdl6nkgitkds", "gv-52cmzh74n26s22.dv.googlehosted.com.")
